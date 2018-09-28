55283313b2f7c3c3bc61f1c933270b7a59855a70
Anoukjaku <Anoukjaku@dhcp-3-26.gde.emn.fr>
2018-09-28 16:01:23 +0200

    test anouk

--- src/tsp/TSPSolver.java
+++ src/tsp/TSPSolver.java
@@ -77,6 +77,7 @@ public class TSPSolver {
 		{
 			// TODO
 			// Code a loop base on time here
+			// Test
 			spentTime = System.currentTimeMillis() - startTime;
 		}while(spentTime < (m_timeLimit * 1000 - 100) );
 		
