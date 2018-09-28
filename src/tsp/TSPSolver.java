f3725e28164ab432a3ae439ab7c34badeaee5d1d
Juliette Gorline <juliette.gorline@gmail.com>
2018-09-28 16:29:39 +0200

    petitcommit

--- src/tsp/TSPSolver.java
+++ src/tsp/TSPSolver.java
@@ -1,7 +1,7 @@
 package tsp;
 
 /**
- * Salut les noobs
+ * Salut les noobs comment ca va ?
  * This class is the place where you should enter your code and from which you can create your own objects.
  * 
  * The method you must implement is solve(). This method is called by the programmer after loading the data.
