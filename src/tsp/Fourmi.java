b816f6ac5929304d082aabfacf3cd24b8331c193
leofa <leofa@172.16.124.238>
2018-10-15 12:21:03 +0200

    leo

--- src/tsp/Fourmi.java
+++ src/tsp/Fourmi.java
@@ -11,29 +11,36 @@ public class Fourmi {
 	private int etat;
 	private int distance;
 	
+	private Colonie colonie;
+	
 	
 	public Fourmi(ArrayList<Integer> villesVisitees, ArrayList<Integer> villesRestantes, int villeActuelle, int etat,
-			int distance) {
+			int distance, Colonie colonie) {
 		this.villesVisitees = villesVisitees;
 		this.villesRestantes = villesRestantes;
 		this.villeActuelle = villeActuelle;
 		this.etat = etat;
 		this.distance = distance;
+		this.colonie = colonie;
 	}
 	
-	public Fourmi() {
-		this(new ArrayList<Integer>(),new ArrayList<Integer>(),0,0,0 );
+	public Fourmi(Colonie colonie) {
+		this(new ArrayList<Integer>(),new ArrayList<Integer>(),0,0,0, colonie);
 		for(int i=0;i<442;i++) {
 			this.villesRestantes.add(i);
 		}
 	}
 	
-	public ArrayList<Integer> prochainesvillesPossibles() {
-		ArrayList<Integer> prochainsvillesPossibles = new ArrayList<Integer>();
+	public ArrayList<Integer> prochainesVillesPossibles() {
+		int critere;
+		//long[][] distances = Colonie.getInstance().getDistance();
+		ArrayList<Integer> prochainesVillesPossibles = new ArrayList<Integer>();
 		for(int ville : this.villesRestantes) {
-						
+			/*if(distances[villeActuelle][ville]<=critere) {
+				prochainesVillesPossibles.add(ville);
+			}*/
 		}
-		return prochainsvillesPossibles;
+		return prochainesVillesPossibles;
 	}
 	
 	
