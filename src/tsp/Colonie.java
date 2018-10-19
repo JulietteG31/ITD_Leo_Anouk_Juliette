039b3843c956d94d113287e216d94a6e00c8da35
Juliette Gorline <juliette.gorline@gmail.com>
2018-10-19 15:09:04 +0200

    commiiiit

--- src/tsp/Colonie.java
+++ src/tsp/Colonie.java
@@ -10,7 +10,7 @@ public class Colonie {
 	 * triangulaire car pheromones[1][2] = pheromones[2][1] 
 	 */
 	
-	public Integer[][] pheromones;
+	public int[][] pheromones;
 	private ArrayList<Fourmi> fourmis;
 	private Instance instance;
 	
@@ -21,21 +21,24 @@ public class Colonie {
 		for(int i = 0; i < nbFourmis; i++) {
 			fourmi = new Fourmi(this);
 			this.fourmis.add(fourmi);
-			
+		} 
+		
+		for(int i = 0; i < instance.getNbCities(); i++) {
+			this.pheromones[i] = new int[instance.getNbCities()-i];
 			// Initialisation des phéromones
-			for(int j = 0; j < i+1; j++) {
+			for(int j = i; j < instance.getNbCities(); j++) {
 				this.pheromones[i][j] = 0;
 			}
-		} 
+		}
 	}
 	
 	public Instance getInstance() {
 		return this.instance;
 	}
-	public Integer[][] getPheromones() {
+	public int[][] getPheromones() {
 		return this.pheromones;
 	}
-	public Integer getPheromones(int i, int j) throws Exception {
+	public int getPheromones(int i, int j) throws Exception {
 		if(i < 0 || j < 0 || i >= this.getInstance().getNbCities() || j >= this.getInstance().getNbCities())
 			throw new Exception("Vous demandez pheromones[i][j] avec des index qui dépassent les limites");
 		
@@ -51,8 +54,12 @@ public class Colonie {
 			this.pheromones[j][i] = value;
 	}
 	
-	/*
-	 * Incrémenter les phéromones d'une valeur inc
+	/**
+	 * Incrémenter les phéromones du chemin entre la ville i et la ville j d'une valeur inc
+	 * @param i
+	 * @param j
+	 * @param inc
+	 * @throws Exception
 	 */
 	public void incPheromones(int i, int j, int inc) throws Exception {
 		if(i < 0 || j < 0 || i >= this.getInstance().getNbCities() || j >= this.getInstance().getNbCities())
