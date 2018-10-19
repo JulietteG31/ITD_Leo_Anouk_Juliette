fb863252b22ea84d9a98975076b58111b322501f
Anoukjaku <Anoukjaku@169.254.67.112>
2018-10-19 14:13:39 +0200

    ,

--- src/tsp/Fourmi.java
+++ src/tsp/Fourmi.java
@@ -39,27 +39,5 @@ public class Fourmi {
 		}
 		return prochainesVillesPossibles;
 	}
-	public int NextStep() {
-		
-		// boucle qui retourne somme (autre)
-		//ponderation de chacune des prochainesVillesPossibles poids=(phero^alpha*(1/d)^Beta)/ somme
-		int i=0;
-		while (i==0) {
-			if(Math.random()<= prochainesVillesPossibles.get(i).poids) {
-				int villeSuivante=prochainesVillesPossibles.get(i);
-				i+=1;
-			}
-			else {
-				prochainesVillesPossibles().remove(0);
-				for (int villesuiv: prochainesVillesPossibles()) {
-					villesuiv.poids=(villesuiv.poids)/(1.0-ville.poids);
-					
-				}
-			}
-		}
-			return villeSuivante;
-		}
-
 	
-
 }
