ba58d7792275998f7bb5c4279caee6bcd412b47f
leofa <leofa@DESKTOP-AOG2SHU>
2018-10-19 15:30:57 +0200

    parcourir

--- src/tsp/Fourmi.java
+++ src/tsp/Fourmi.java
@@ -37,9 +37,7 @@ public class Fourmi {
 	 */
 	public Fourmi(Colonie colonie) {
 		this(new ArrayList<Integer>(),new ArrayList<Integer>(),0,0,0, colonie);
-		for(int i=0;i<442;i++) {
-			this.villesRestantes.add(i);
-		}
+		this.initialiser();
 	}
 	
 	/**
@@ -145,7 +143,10 @@ public class Fourmi {
 	 * @throws Exception
 	 */
 	public boolean arriveeADestination() throws Exception {
-		if(this.villesRestantes.size()==0 && this.villeActuelle==0) {
+		if(this.villesRestantes.size()==0) {
+			// On ferme la boucle du trajet
+			this.avancer(0);
+			
 			this.etat=2;
 			return true;
 		}
@@ -154,6 +155,23 @@ public class Fourmi {
 		}
 	}
 	
+	/**
+	 * Initialise une fourmi (ou la réinitialise)
+	 */
+	public void initialiser() {
+		this.villeActuelle = 0;
+		this.etat = 0;
+		this.distance = 0;
+		this.villesVisitees = new ArrayList();
+		
+		for(int i=0;i<this.colonie.getInstance().getNbCities();i++) {
+			this.villesRestantes.add(i);
+		}
+	}
+	
+	public int[] mettreAJourMeilleurChemin() {
+		return null;
+	}
 	
 	/**
 	 * Description : défini comment la fourmi doit se déplacer dans le graphe.
@@ -161,10 +179,14 @@ public class Fourmi {
 	 */
 	public void parcourir() throws Exception {
 		if(this.arriveeADestination()) {
-			
+			this.deposerPheromones();
+			this.mettreAJourMeilleurChemin();
+			this.initialiser();
+			this.parcourir();
 		}
 		else {
 			
 		}
 	}
 } 
+
